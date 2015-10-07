ed7317d38b93eb1b8f2f321b4d7f185397807459
SenYu@ThinkMac <senyu@mail.dlut.edu.cn>
2015-10-08 00:42:39 +0800

    [Feature] 加入README，配置部署脚本

---
+++ scripts/deploy_production.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "enter deploy_wrap"
+
+DEPLOYPROJECTNAME="nclientAsset"
+NEWVERSION=$(git rev-parse --short HEAD)
+DEPLOYCOPY="~/DEPLOY/"
+DEPLOYDEST="/home/wwwroot/asset.senyu.me/public_html/knewsAsset/prod/deploy/"
+DEPLOYFILENAME=deploy_${DEPLOYPROJECTNAME}_${NEWVERSION}.tar.gz
+DEPLOYSERVER="deploy@senyu.me"
+DEPLOYMODE="PRODUCTION"
+
+echo $DEPLOYPROJECTNAME
+echo $NEWVERSION
+echo $DEPLOYCOPY
+echo $DEPLOYDEST
+echo $DEPLOYFILENAME
+echo $DEPLOYSERVER
+
+cd build
+tar -zcf ../$DEPLOYFILENAME *
+
+scp -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ../${DEPLOYFILENAME} ${DEPLOYSERVER}:${DEPLOYCOPY}
+#ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 $DEPLOYSERVER "ls -l $DEPLOYCOPY;"
+ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ${DEPLOYSERVER} "${DEPLOYCOPY}/remote_execute_asset.sh ${NEWVERSION} ${DEPLOYCOPY} ${DEPLOYDEST} ${DEPLOYPROJECTNAME} ${DEPLOYFILENAME} ${DEPLOYMODE}"
